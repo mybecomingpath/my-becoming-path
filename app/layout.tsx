8eb2810d18a6fbdf03b925733bb16212b8754937
mybecomingpath <chris@empoweredgriefjourney.com>
2026-07-11 16:09:05 -0700

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./navbar";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -38,6 +39,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
   <Navbar/>
   {children}
+  <GoogleAnalytics gaId="G-QPK1VNZHRN" />
 </body>
     </html>
   );

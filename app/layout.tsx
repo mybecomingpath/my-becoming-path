e3777fe1e6dbc8a569599d420b7c37a8cb52c3c6
mybecomingpath <chris@empoweredgriefjourney.com>
2026-09-01 10:46:27 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,10 +37,12 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-  <Navbar/>
-  {children}
-  <GoogleAnalytics gaId="G-QPK1VNZHRN" />
-</body>
+        <Navbar />
+
+        {children}
+
+        <GoogleAnalytics gaId="G-QPK1VNZHRN" />
+      </body>
     </html>
   );
 }
